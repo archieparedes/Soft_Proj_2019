aac885bb98c365f2fc654800f92745a7735d8528
archie paredes <archieparedes@gmail.com>
2019-02-12 11:35:40 -0600

    reupload soon

--- indeed/indeed_URLs.py
+++
@@ -1,9 +0,0 @@
-"""
-@author Archie_Paredes
-@created JAN 26, 2018
-@version 1.0
-Indeed API - URL puller
-"""
-import csv
-import json
-from indeed_api_key import token, secret # api keys to access indeed api
--- indeed/indeed_word_cloud.py
+++
@@ -1,6 +0,0 @@
-"""
-@author Archie_Paredes
-@created JAN 26, 2018
-@version 1.0
-Indeed API - Word Cloud
-"""
