e1de25d88661145526c80b7a6d42275811dce60c
Octavian <stoch.octavian@gmail.com>
2019-02-26 19:18:40 -0600

    get all of the individual job links, now rating and avg salary can be gotten. Still problem running through all links in main glassdoor.py file

--- getLinks.py
+++ getLinks.py
@@ -2,9 +2,13 @@ from bs4 import BeautifulSoup
 import urllib.request
 from requests_html import HTMLSession
 import re
+import requests
+from lxml import html
 
-urlList = []
-urlList2 = []
+jobDescriptionURL = []
+secondListURL = []
+formattedURL = []
+absoluteURL = 'http://glassdoor.com'
 
 #change the user agent so that we dont get a 403 error
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
@@ -18,6 +22,40 @@ data = response.read() #this is the raw html in case it's needed
 soup = BeautifulSoup(data, "lxml")
 soup.prettify()
 
-for foo in soup.findAll('a', href = re.compile('/partner+')):
-	print (foo['href'])
-	urlList.append(foo['href'])
+def get_all_job_links(): #condense all of the link creation here
+	for foo in soup.findAll('a', href = re.compile('/partner+')):
+		jobDescriptionURL.append(foo['href'])
+	for foo in jobDescriptionURL:
+		secondListURL.append(absoluteURL + foo)
+	for foo in secondListURL:
+		stuff = foo.strip()
+		formattedURL.append(stuff)
+	
+get_all_job_links()
+
+for x in formattedURL:
+	print (x)
+
+sessions = HTMLSession()
+r = sessions.get(formattedURL[4])
+print(r.status_code) #sanity check
+
+XPATH_NAME = '//*[@class="jobViewJobTitleWrap"]'
+XPATH_COMPANY = '//*[@class="strong ib"]'
+XPATH_LOC = '//*[@class="subtle ib"]'
+XPATH_SALARY = ".//*[@class='salEst']"
+XPATH_EVERYTHING = '//*'
+XPATH_RATING = '//*[@class="jobListingRating"]'
+
+
+response = sessions.get(formattedURL[4])
+print(response)
+tree = html.fromstring(response.content)
+name = tree.xpath(XPATH_NAME)
+print (name)
+company = tree.xpath(XPATH_COMPANY)
+print (company)
+rating = tree.xpath(XPATH_RATING)
+print (rating)
+
+
--- glassdoor.py
+++ glassdoor.py
@@ -11,6 +11,8 @@ from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from getLinks import get_all_job_links
+import urllib.request
 
 #These are the google credentials and links for connecting to the sheets API
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
@@ -20,30 +22,55 @@ client = gspread.authorize(creds)
 # #name of sheet in my google sheets
 sheet = client.open('glassdoor Jobs')
 
+jobDescriptionURL = []
+secondListURL = []
+formattedURL = []
+absoluteURL = 'http://glassdoor.com'
 
+#change the user agent so that we dont get a 403 error
+user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+	
+url = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=true&clickSource=searchBtn&typedKeyword=Android&sc.keyword=Android+Developer&locT=C&locId=1128808&jobType="
+headers = {'User-Agent':user_agent,} #apply the user agent here
 
-url = ('https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=android+developer&sc.keyword=android+developer&locT=C&locId=1128808&jobType=')
+request = urllib.request.Request(url,None,headers)
+response = urllib.request.urlopen(request)
+data = response.read() #this is the raw html in case it's needed
+soup = BeautifulSoup(data, "lxml")
+soup.prettify()
+
+def get_all_job_links(): #condense all of the link creation here
+	for foo in soup.findAll('a', href = re.compile('/partner+')):
+		jobDescriptionURL.append(foo['href'])
+	for foo in jobDescriptionURL:
+		secondListURL.append(absoluteURL + foo)
+	for foo in secondListURL:
+		stuff = foo.strip()
+		formattedURL.append(stuff)
+	
+get_all_job_links()
+
 
 sessions = HTMLSession()
-r = sessions.get(url)
-print(r.status_code)
+r = sessions.get(formattedURL[4])
+print (' this is a sanity check')
+print(r.status_code) #sanity check
 
 
 
 #xpath declaration
-XPATH_NAME = './/a/text()'
-XPATH_COMPANY = './/div[@class="flexbox empLoc"]/div/text()'
-XPATH_LOC = './/span[@class="subtle loc"]/text()'
-XPATH_SALARY = '//*[@id="Details"]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div/text()'
-XPATH_EVERYTHING = '//li[@class="jl"]'
-XPATH_RATING = '//a[contains(@class,"avg")]/text()'
-
-
+XPATH_NAME = '//*[@class="jobViewJobTitleWrap"]'
+XPATH_COMPANY = '//*[@class="strong ib"]'
+XPATH_LOC = '//*[@class="subtle ib"]'
+XPATH_SALARY = ".//*[@class='salEst']"
+XPATH_EVERYTHING = '//*'
+XPATH_RATING = '//*[@class="empStatsBody"]'
 
 #try except block to catch exceptions
 try:
+	for my_urls in formattedURL:
 		theBiggestList = []
-		response = sessions.get(url)
+		response = sessions.get(my_urls)
 		print(response)
 		parser = html.fromstring(response.content)
 
@@ -60,6 +87,7 @@ try:
 			
 			
 			print(foo_name)
+			print(foo_company)
 			print(foo_citystate)
 			print(foo_rating)
 			print(foo_money)
@@ -80,7 +108,7 @@ try:
 				"Rating" : foo_rating_cleaned
 			}
 			theBiggestList.append(myDict)
-		print("works??")
+	print("works??")
 except:
 	print("Something went wrong")
 	  
