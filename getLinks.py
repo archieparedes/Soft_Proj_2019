5aaf93a038912f04eb51e567117862cf24d5f6aa
Octavian <stoch.octavian@gmail.com>
2019-02-28 18:13:26 -0600

    update geLinks.py if a rating and salary is shown it will display

--- getLinks.py
+++ getLinks.py
@@ -13,7 +13,7 @@ absoluteURL = 'http://glassdoor.com'
 #change the user agent so that we dont get a 403 error
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 	
-url = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=true&clickSource=searchBtn&typedKeyword=Android&sc.keyword=Android+Developer&locT=C&locId=1128808&jobType="
+url = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=Android+Developer&sc.keyword=Android+Developer&locT=C&locId=1128808&jobType="
 headers = {'User-Agent':user_agent,} #apply the user agent here
 
 request = urllib.request.Request(url,None,headers)
@@ -30,32 +30,42 @@ def get_all_job_links(): #condense all of the link creation here
 	for foo in secondListURL:
 		stuff = foo.strip()
 		formattedURL.append(stuff)
-	
+		
 get_all_job_links()
+
+
+count = 0
 
 for x in formattedURL:
 	print (x)
+	count +=1
+print('There are: ',count, ' total URLs')	
+print('\n\n\n\n\n')
 
 sessions = HTMLSession()
 r = sessions.get(formattedURL[4])
 print(r.status_code) #sanity check
 
-XPATH_NAME = '//*[@class="jobViewJobTitleWrap"]'
-XPATH_COMPANY = '//*[@class="strong ib"]'
-XPATH_LOC = '//*[@class="subtle ib"]'
-XPATH_SALARY = ".//*[@class='salEst']"
+XPATH_NAME = '//*[@class="jobViewJobTitleWrap"]//text()'
+XPATH_COMPANY = '//*[@class="strong ib"]//text()'
+XPATH_LOC = '//*[@class="subtle ib"]//text()'
+XPATH_SALARY = ".//*[@class='salEst']//text()"
 XPATH_EVERYTHING = '//*'
-XPATH_RATING = '//*[@class="jobListingRating"]'
+XPATH_RATING = '//*[@class="ratingNum"]//text()'
 
 
-response = sessions.get(formattedURL[4])
-print(response)
-tree = html.fromstring(response.content)
-name = tree.xpath(XPATH_NAME)
-print (name)
-company = tree.xpath(XPATH_COMPANY)
-print (company)
-rating = tree.xpath(XPATH_RATING)
-print (rating)
-
-
+for this_url in formattedURL:	
+	response = sessions.get(this_url)
+	print(response)
+	tree = html.fromstring(response.content)
+	name = tree.xpath(XPATH_NAME)
+	print ('This is the job name: ', name)
+	company = tree.xpath(XPATH_COMPANY)
+	print ('This is the company: ', company)
+	rating = tree.xpath(XPATH_RATING)
+	print ('This is the overall rating: ', rating)
+	salary = tree.xpath(XPATH_SALARY)
+	print ('This is the average salary: ', salary)
+	location = tree.xpath(XPATH_LOC)
+	print ('This is the city and state: ', location)
+	
