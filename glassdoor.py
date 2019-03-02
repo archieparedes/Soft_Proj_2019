c8f3a528646c77a42f942d583f93189a7e905dc8
Octavian <stoch.octavian@gmail.com>
2019-03-02 13:52:46 -0600

    modified variables names so that its easier to read, and improved way of getting data (data not cleaned,yet)

--- glassdoor.py
+++ glassdoor.py
@@ -11,7 +11,6 @@ from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from getLinks import get_all_job_links
 import urllib.request
 
 #These are the google credentials and links for connecting to the sheets API
@@ -30,7 +29,7 @@ absoluteURL = 'http://glassdoor.com'
 #change the user agent so that we dont get a 403 error
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 	
-url = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=true&clickSource=searchBtn&typedKeyword=Android&sc.keyword=Android+Developer&locT=C&locId=1128808&jobType="
+url = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=Android+Developer&sc.keyword=Android+Developer&locT=C&locId=1128808&jobType="
 headers = {'User-Agent':user_agent,} #apply the user agent here
 
 request = urllib.request.Request(url,None,headers)
@@ -49,7 +48,11 @@ def get_all_job_links(): #condense all of the link creation here
 		formattedURL.append(stuff)
 	
 get_all_job_links()
-
+num_links = 0
+for x in formattedURL:
+	print (x)
+	num_links+=1
+print ('There are: ',num_links, ' number of links')
 
 sessions = HTMLSession()
 r = sessions.get(formattedURL[4])
@@ -59,59 +62,53 @@ print(r.status_code) #sanity check
 
 
 #xpath declaration
-XPATH_NAME = '//*[@class="jobViewJobTitleWrap"]'
-XPATH_COMPANY = '//*[@class="strong ib"]'
-XPATH_LOC = '//*[@class="subtle ib"]'
-XPATH_SALARY = ".//*[@class='salEst']"
+XPATH_NAME = '//*[@class="jobViewJobTitleWrap"]//text()'
+XPATH_COMPANY = '//*[@class="strong ib"]//text()'
+XPATH_LOC = '//*[@class="subtle ib"]//text()'
+XPATH_SALARY = ".//*[@class='salEst']//text()"
 XPATH_EVERYTHING = '//*'
-XPATH_RATING = '//*[@class="empStatsBody"]'
+XPATH_RATING = '//*[@class="ratingNum"]//text()'
+#try except block to catch exceptions (not anymore)
 
-#try except block to catch exceptions
-try:
-	for my_urls in formattedURL:
-		theBiggestList = []
-		response = sessions.get(my_urls)
-		print(response)
-		parser = html.fromstring(response.content)
+theBiggestList = []
+for this_url in formattedURL:	
+	response = sessions.get(this_url)
+	'''parser = html.fromstring(response.content)
 
-		absolute_url = "https://www.glassdoor.com"
-		parser.make_links_absolute(absolute_url)
+	absolute_url = "https://www.glassdoor.com"
+	parser.make_links_absolute(absolute_url)
 
-		bar = parser.xpath(XPATH_EVERYTHING)
-		for foo in bar:
-			foo_name = foo.xpath(XPATH_NAME)
-			foo_company = foo.xpath(XPATH_COMPANY)
-			foo_money = foo.xpath(XPATH_SALARY)
-			foo_citystate = foo.xpath(XPATH_LOC)
-			foo_rating = foo.xpath(XPATH_RATING)
-			
-			
-			print(foo_name)
-			print(foo_company)
-			print(foo_citystate)
-			print(foo_rating)
-			print(foo_money)
-			
-			#########
-			#Clean up the dirt
-			foo_name_cleaned = ''.join(foo_name).strip('-') if foo_name else None
-			foo_company_cleaned = ''.join(foo_company).replace('-','')
-			foo_money_cleaned = ''.join(foo_money).strip()
-			foo_citystate_cleaned = ''.join(foo_citystate).strip('-')
-			foo_rating_cleaned = ''.join(foo_rating).strip()
+	bar = parser.xpath(XPATH_EVERYTHING)
+	for foo in bar:'''
+	print(response)
+	tree = html.fromstring(response.content)
+	name = tree.xpath(XPATH_NAME)
+	print ('This is the job name: ', name)
+	company = tree.xpath(XPATH_COMPANY)
+	print ('This is the company: ', company)
+	rating = tree.xpath(XPATH_RATING)
+	print ('This is the overall rating: ', rating)
+	salary = tree.xpath(XPATH_SALARY)
+	print ('This is the average salary: ', salary)
+	location = tree.xpath(XPATH_LOC)
+	print ('This is the city and state: ', location)
+		
+	#########
+	#Clean up the dirt
+	name_cleaned = ''.join(name).strip('-') if name else None
+	company_cleaned = ''.join(company).replace('-','')
+	salary_cleaned = ''.join(salary).strip()
+	location_cleaned = ''.join(location).strip('-')
+	rating_cleaned = ''.join(rating).strip()
 
-			myDict = {
-				"Name" : foo_name_cleaned,
-				"Company" : foo_company_cleaned,
-				"Salary" : foo_money_cleaned,
-				"Location" : foo_citystate_cleaned,
-				"Rating" : foo_rating_cleaned
-			}
-			theBiggestList.append(myDict)
-	print("works??")
-except:
-	print("Something went wrong")
-	  
+	myDict = {
+		"Name" : name,
+		"Company" : company,
+		"Salary" : salary,
+		"Location" : location,
+		"Rating" : rating
+	}
+	theBiggestList.append(myDict)	  
 print(theBiggestList)
 
 
